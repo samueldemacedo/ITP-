aa177e1bb4f55bd67da336b5a57eaccbb35e0afc
Samuel Macêdo <samuelmacedodasilva2020@gmail.com>
2025-10-07 15:15:44 -0300

    Problema_2
    Conversor de temperatura.
    De graus Celsius para Fahrenheit e Kelvin.

---
+++ Semana_2/main (1).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    
+    float celsius, fahrenheit, kelvin;
+    
+    printf("Esse programa converte graus Celsius em Fahrenheit e Kelvin.\n");
+    printf("Digite a temperatura em graus Celsius: \n");
+    scanf("%f", &celsius);
+    
+    fahrenheit = (celsius*9/5) + 32;
+    kelvin = celsius + 273.15;
+    
+    printf("%.1f graus Celsius.\n", celsius);
+    printf("%.1f graus Fahrenheit.\n", fahrenheit);
+    printf("%.1f graus Kelvin.", kelvin);
+    
+    }

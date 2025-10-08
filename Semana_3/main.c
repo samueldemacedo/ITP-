c7031ae4facc3403c192f254c4b6aecf7197a454
Samuel Macêdo <samuelmacedodasilva2020@gmail.com>
2025-10-08 19:49:20 -0300

    add, problema 2 - lista semana 3
    adicionando resolução do problema 2 da lista da terceira semana

---
+++ Semana_3/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+ //fazer um programa que leia a média de 3 notas com o if//
+ 
+ int main(){
+     
+     float n1, n2, n3;
+     float media;
+     
+     printf("Digite a primeira nota:\n");
+     scanf("%f", &n1);
+     
+     printf("Digite a segunda nota:\n");
+     scanf("%f", &n2);
+     
+     printf("Digite a terceira nota:\n");
+     scanf("%f", &n3);
+     
+     media = (n1 + n2 + n3) / 3;
+     
+     if (media < 4.0){
+         printf("Reprovado");
+     }
+     
+     else if (media >= 4.0 && media < 7.0){
+         printf("Recuperação");
+     }
+     
+     else {
+         printf ("Aprovado");
+     }
+     
+     return 0;
+     
+     }
